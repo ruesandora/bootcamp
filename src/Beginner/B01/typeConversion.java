dff1070be82f2f9b0cbb3294f49038582435216d
rues <ruesforum@gmail.com>
2023-07-19 20:11:09 +0300

    typeConversion

--- src/Beginner/B01/typeConversion.java
+++ src/Beginner/B01/typeConversion.java
@@ -2,6 +2,17 @@ package Beginner.B01;
 
 public class typeConversion {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String a = "5"; // String = 5
+
+        // I did type conversion of the String value in variable a to b
+        int b = Integer.valueOf(a);
+        System.out.println(b); // 5
+
+        // I Stringified the int value in the ab variable.
+        int ab = 21;
+        String abc = String.valueOf(ab); // 21
+        System.out.println(abc); // 21
+
+
     }
 }
