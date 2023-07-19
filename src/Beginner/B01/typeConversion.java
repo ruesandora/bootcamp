d5e48b95b3d56aebec7b0ab226ea74aa1170d43e
rues <ruesforum@gmail.com>
2023-07-19 20:24:46 +0300

    typeConversion

--- src/Beginner/B01/typeConversion.java
+++ src/Beginner/B01/typeConversion.java
@@ -2,9 +2,9 @@ package Beginner.B01;
 
 public class typeConversion {
     public static void main(String[] args) {
-        String a = "5"; // String = 5
 
         // I did type conversion of the String value in variable a to b
+        String a = "5"; // String = 5
         int b = Integer.valueOf(a);
         System.out.println(b); // 5
 
@@ -13,6 +13,15 @@ public class typeConversion {
         String abc = String.valueOf(ab); // 21
         System.out.println(abc); // 21
 
+        // I cast double to int.
+        double d = 5;
+        int dd = (int) d; // data casting ==> 5
+        System.out.println(dd);
+
+        // I cast int to double.
+        int ii = 10;
+        double id = (double) ii;
+        System.out.println(id);
 
     }
 }
