0217e101f6db3f5a58e3d8a08519eb65fa90c3cb
rues <ruesforum@gmail.com>
2023-07-20 16:52:24 +0300

    4

---
+++ src/Beginner/B01/Arrays.java
@@ -0,0 +1,17 @@
+package Beginner.B01;
+
+import java.util.SortedMap;
+
+public class Arrays {
+    public static void main(String[] args) {
+        System.out.println("DİZİNİN ELEMANLARI:");
+        System.out.println();
+
+        String[] isimler = {"Ahmet", "Mehmet", "Ayşe"};
+
+        for (int i = 0; i < isimler.length; i++) {
+            System.out.println(isimler[i]);
+        }
+
+    }
+}
