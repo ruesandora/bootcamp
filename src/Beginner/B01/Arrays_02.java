a7c649fb1310b070b0baaf292cd4e489f8b5d68b
rues <ruesforum@gmail.com>
2023-07-20 17:17:52 +0300

    6

---
+++ src/Beginner/B01/Arrays_02.java
@@ -0,0 +1,29 @@
+package Beginner.B01;
+
+import java.util.Scanner;
+
+public class Arrays_02 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Dizi kaç elamanlı olsun?: ");
+        int elemanSayisi = scan.nextInt();
+        scan.nextLine();
+        System.out.println();
+
+        String[] elemanlar = new String[elemanSayisi];
+
+        for (int i = 0; i < elemanSayisi; i++) {
+            System.out.print("Dizini " + i + ". elamanını giriniz..: ");
+            elemanlar[i] = scan.nextLine();
+        }
+
+        System.out.println();
+        System.out.println("Elemanlarınız:");
+        System.out.println();
+
+        for (int i = 0; i < elemanSayisi; i++) {
+            System.out.println(i + "." + " Elemanınız: " + elemanlar[i]);
+        }
+    }
+}
