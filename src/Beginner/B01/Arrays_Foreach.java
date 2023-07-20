36534b696bcb4258f329d1c1e90c975b48fb57fd
rues <ruesforum@gmail.com>
2023-07-20 23:59:22 +0300

    10

--- src/Beginner/B01/Arrays_Foreach.java
+++ src/Beginner/B01/Arrays_Foreach.java
@@ -28,21 +28,21 @@ public class Arrays_Foreach {
             scan.nextLine(); // int veri girildikten sonra enter için girilen değeri yakalama.
         }
 
-        for (int sayi: sayilar) {
-            for (int j = 2; j < sayi; j++) {
-                if (sayi % j == 0){
-                    asalOlmayanToplam += 1;
-                    asalOlmayanSayilar += String.valueOf(sayi) + " ";
-                    asalMi = false;
-                    break;
+        for (int sayi: sayilar) { // sayilar arrayinde ki tüm sayıların uzunluğunu sayi variable'ına atar
+            for (int j = 2; j < sayi; j++) { // j ==> 2'den başlar sayi'ya kadar.
+                if (sayi % j == 0){ // sayi % j bölümü 0'ı buluyorsa bunun asal olmadığını belirleyen bir döngü.
+                    asalOlmayanToplam += 1; // asal olmayan kaç sayı varsa girilir.
+                    asalOlmayanSayilar += String.valueOf(sayi) + " "; // asal olmayan tüm sayılar yazdırılır.
+                    asalMi = false; // asal olmadığını belirten false flagı
+                    break; // for döngüsünü kırmak için. (asalsa yazdırıp değerler girilecek, değilse else bloğuna geçilir.)
                 } else {
-                    asalMi = true;
+                    asalMi = true; // yukarıda ki sayı asal ise else bloğuna geçilir ve flagı true'ya çevirir.
                 }
             }
 
-            if (asalMi){
-                asalToplam += 1;
-                asalSayilar += String.valueOf(sayi) + " ";
+            if (asalMi){ // flag'ın true'ya çevrilmesi durumunda çalışacak koşul
+                asalToplam += 1; // asal olan kaç sayı varsa girilir.
+                asalSayilar += String.valueOf(sayi) + " "; // asal olan tüm sayılar yazdırılır.
                 asalMi = false;
             }
         }
