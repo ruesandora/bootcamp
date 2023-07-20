b8bb084246624879da0366615e76aa4b876334bd
rues <ruesforum@gmail.com>
2023-07-20 23:40:46 +0300

    9

--- src/Beginner/B01/ArrayList.java
+++
@@ -1,7 +0,0 @@
-package Beginner.B01;
-
-public class ArrayList {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/Beginner/B01/ArrayListOlusturma.java
@@ -0,0 +1,14 @@
+package Beginner.B01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListOlusturma {
+    public static void main(String[] args) {
+
+        List<String> isimler = new ArrayList<>();
+        isimler.add("Ayşe");
+        isimler.add("Fatma");
+        System.out.println(isimler); // [Ayşe, Fatma]
+    }
+}
