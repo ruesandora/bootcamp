c7d798fa3cdd963744594f6b240699f0e42dd4a6
rues <ruesforum@gmail.com>
2023-07-22 01:07:21 +0300

    12

---
+++ src/Beginner/B01/Method.java
@@ -0,0 +1,25 @@
+package Beginner.B01;
+
+public class Method {
+    public static void main(String[] args) {
+
+        // parametresiz ve geriye değer döndürmeyen Method tanımlama.
+
+        isimYazdir();
+
+    }
+
+    /*
+       * erisim_belirleyicisi donus_tipi adi()
+       *
+       * Methodlar size hizmet eden kod blocklarıdır.
+       * Bir kere yazar her yerde kullanabilirsiniz.
+       *
+       *
+       *
+     */
+
+    public static void isimYazdir(){
+        System.out.println("Vitalik");
+    }
+}
