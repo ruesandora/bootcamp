48709e5b7158e36f6fb66edff67ab5d39c47a8ce
rues <ruesforum@gmail.com>
2023-07-22 01:19:33 +0300

    13

--- src/Beginner/B01/Method.java
+++ src/Beginner/B01/Method.java
@@ -3,22 +3,24 @@ package Beginner.B01;
 public class Method {
     public static void main(String[] args) {
 
-        // parametresiz ve geriye değer döndürmeyen Method tanımlama.
-
-        isimYazdir();
+         isimYazdir();
+         topla(957,312);
+         isimVeSoyisim("Vitalik" , "Uniswapoğlu");
 
     }
 
-    /*
-       * erisim_belirleyicisi donus_tipi adi()
-       *
-       * Methodlar size hizmet eden kod blocklarıdır.
-       * Bir kere yazar her yerde kullanabilirsiniz.
-       *
-       *
-       *
-     */
+    public static void isimVeSoyisim(String isim, String soyisim){
 
+        System.out.println();
+        System.out.println("İsminiz: " + isim);
+        System.out.println("Soyisminiz: " + soyisim);
+        System.out.println("İsim ve Soyisim: " + isim.concat(" " + soyisim));
+    }
+
+    public static void topla(int num1, int num2){
+        System.out.println();
+        System.out.println("Sayıların toplamı: " + (num1 + num2));
+    }
     public static void isimYazdir(){
         System.out.println("Vitalik");
     }
