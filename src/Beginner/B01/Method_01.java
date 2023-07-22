65a61115b1e309ced7cf1f8ced596729d357b6af
rues <ruesforum@gmail.com>
2023-07-22 16:11:19 +0300

    14

--- src/Beginner/B01/Method_01.java
+++ src/Beginner/B01/Method_01.java
@@ -1,7 +1,34 @@
 package Beginner.B01;
 
+import java.util.Scanner;
+
 public class Method_01 {
     public static void main(String[] args) {
 
+        System.out.println("SAYININ TEK Mİ ÇİFT Mİ OLDUĞUNU BULMA PROGRAMI");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dizinin eleman sayısını giriniz: ");
+        int diziLength = scanner.nextInt();
+        scanner.nextLine();
+
+        int[] sayilar = new int[diziLength];
+
+        for (int i = 0; i <sayilar.length ; i++) {
+            System.out.println("Dizinin " + i + ". index değerini giriniz: ");
+            sayilar[i] = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        for (int deger: sayilar) {
+            tekMiCiftMi(deger);
+        }
+    }
+
+    public static void tekMiCiftMi(int sayi){
+
+        if (sayi % 2 == 0){
+            System.out.println("Sayi : " + sayi + " ÇİFTTİR");
+        } else System.out.println("Sayi : " + sayi + " TEKTİR" );
     }
 }
