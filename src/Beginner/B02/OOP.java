5480118e67c3d52bbc7dadded8664d67f08f45f7
rues <ruesforum@gmail.com>
2023-07-25 01:46:29 +0300

    16

--- src/Beginner/B02/OOP.java
+++ src/Beginner/B02/OOP.java
@@ -3,14 +3,19 @@ package Beginner.B02;
 public class OOP {
     public static void main(String[] args) {
 
-        Obje ogrenci1 = new Obje();
+        // Constructor (yapıcı metot)
 
-        ogrenci1.id = 10;
-        ogrenci1.isim = "Vitalik";
-        ogrenci1.soyisim = "Buterin";
+
+        Obje ogrenci1 = new Obje(7, "Ali", "Çağdaş");
+        Obje ogrenci2 = new Obje();
+
+//        ogrenci1.id = 10;
+//        ogrenci1.isim = "Vitalik";
+//        ogrenci1.soyisim = "Buterin";
 
         System.out.println("Öğrenci ID'si: " + ogrenci1.id);
         System.out.println("Öğrenci ismi: " + ogrenci1.isim);
         System.out.println("Öğrenci soyismi: " + ogrenci1.soyisim);
+        
     }
 }
--- src/Beginner/B02/Obje.java
+++ src/Beginner/B02/Obje.java
@@ -5,4 +5,20 @@ public class Obje {
     int id;
     String isim;
     String soyisim;
+
+    public Obje (int id, String isim, String soyisim){
+        // this : şuan üzerinde çalıştığım class
+
+        this.id = id;
+        this.isim = isim;
+        this.soyisim=soyisim;
+    }
+
+    public Obje (){
+        System.out.println("test");
+    }
+
+    public Obje (int id){
+    }
+
 }
