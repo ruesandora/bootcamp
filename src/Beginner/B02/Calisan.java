5bd51e5d2b7c4996b118ea8a5794c3aba7a58301
rues <ruesforum@gmail.com>
2023-07-27 18:56:45 +0300

    12

---
+++ src/Beginner/B02/Calisan.java
@@ -0,0 +1,38 @@
+package Beginner.B02;
+
+public class Calisan {
+  public int no;
+  public String isim;
+  public String soyisim;
+  public double maas;
+  public int tecrube;
+
+  public Calisan(){
+
+  }
+  public Calisan(int no, String isim, String soyisim, int tecrube, double maas){
+    this.no = no;
+    this.isim = isim;
+    this.soyisim = soyisim;
+    this.maas = maas;
+    this.tecrube = tecrube;
+  }
+
+  public void calisanBilgileriGoster(){
+    System.out.println("****CALISAN BILGILERI****");
+    System.out.println("No: " + no);
+    System.out.println("İsim: " + isim);
+    System.out.println("Soyisim: " + soyisim);
+    System.out.println("Tecrübe: " + tecrube);
+  }
+
+  public void zamYap(int zamDegeri){
+    System.out.println("Maaşınıza " + zamDegeri + "TL zam yapıldı");
+    System.out.println("Güncel maaşınız: " + (maas+zamDegeri));
+  }
+
+  public void formatAt(String isletimSistemi, String kim){
+    System.out.println(kim + " şu anda " + isletimSistemi + " işletim sistemine format atıyor");
+  }
+
+}
---
+++ src/Beginner/B02/OOP_Test.java
@@ -0,0 +1,10 @@
+package Beginner.B02;
+
+public class OOP_Test {
+  public static void main(String[] args) {
+
+    // Calisan sınıfını olusturup ise baslayalım.
+
+
+  }
+}
